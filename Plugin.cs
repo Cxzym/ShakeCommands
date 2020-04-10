efedbde1322d79c71f64dc6da34addc6df4873e0
Wade <49218779+Cxzym@users.noreply.github.com>
2020-04-10 19:53:31 -0400

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using TShockAPI;
+using TerrariaApi.Server;
+using Terraria;
+using Newtonsoft.Json;
+using Microsoft.Xna.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using Terraria.DataStructures;
+using TShockAPI.Hooks;
+
+namespace ShakePlugin.cs
+{
+    [ApiVersion ( 2, 1 )]
+
+    public class ShakePlugin : TerrariaPlugin
+    {
+
+        public override string Name => "ShakePlugin";
+        public override Version Version => new Version();
+        public override string Author => "Wade";
+        public override string Description => "This command will allow you to shake hands with other users.";
+
+        public ShakePlugin(Main game) : base(game) { }
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+
+        private void OnInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("shakecommand.shake", Shake, "shake")
+            {
+                HelpText = "Shakes hands with another user."
+            }); 
+        }
+        private void Shake(CommandArgs args)
+        {
+            string playerName = args.Parameters[0];
+            
+            var players = TShock.Utils.FindPlayer(playerName);
+            
+            if (players.Count == 0)
+            {
+                args.Player.SendErrorMessage("Invalid player!");
+            }
+            
+            else if (players.Count > 1)
+            {
+                TShock.Utils.SendMultipleMatchError(args.Player,players.Select(p => p.Name));
+            }
+            
+            else
+            {
+                args.Player.SendInfoMessage($"You shook hands with {players[0].Name}.");
+
+                TSPlayer.All.SendSuccessMessage($"{args.Player.Name} shook hands with {players[0].Name}!");
+            }
+        }
+       
+    }
+}
+
+
+
